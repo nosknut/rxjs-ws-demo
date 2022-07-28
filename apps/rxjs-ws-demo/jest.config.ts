c80b313007293ad428f0d06724dd1af80eb81f14
nosknut <nosknut@gmail.com>
2022-07-28 17:13:26 +0200

    initial commit

---
+++ apps/rxjs-ws-demo/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'rxjs-ws-demo',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/rxjs-ws-demo',
+};
