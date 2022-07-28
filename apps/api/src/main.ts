63f1ef75153c8fa776f666a93522f4ddab1cea74
nosknut <nosknut@gmail.com>
2022-07-28 20:31:42 +0200

    Adds UI

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,46 +5,64 @@
 
 import * as express from 'express';
 import { createServer } from 'http';
-import socketIo from 'socket.io';
+import { Server as SocketIoServer } from 'socket.io';
 import * as uuid from 'uuid';
-import wait from 'wait';
+
+import cors = require('cors');
+import delay = require('delay');
 
 const app = express();
 const server = createServer(app);
-const io = socketIo(server);
+const io = new SocketIoServer(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
 const activeJobProgress: {
-  [id: string]: number | undefined
-} = {}
+  [id: string]: number | undefined;
+} = {};
 
 const computeProgress = (current: number, total: number) => {
-  return Number(((current/total)*100).toFixed(1));
-}
+  return Number(((current / total) * 100).toFixed(1));
+};
 
 const reportJobProgress = (id: string, progress: number) => {
   activeJobProgress[id] = progress;
-  const topic = `api/jobs/${id}/progress`;
-  io.emit(topic, { progress });
-}
+  const topic = `api/jobs/progress`;
+  io.emit(topic, {
+    id,
+    progress,
+  });
+};
 
 const startTimedJob = async (id: string, times: number) => {
-  for (let i = 0; i < times; i++) {
-    await wait(1000);
+  for (let i = 0; i < times; i += 5) {
+    await delay(1000);
+    console.log('update');
     reportJobProgress(id, computeProgress(i, times));
   }
-}
+  reportJobProgress(id, 100);
+};
 
 // post endpoint that accepts a new job
 app.post('/api/jobs', (req, res) => {
   // get an id for the job using the uuid library
   const id = uuid.v4();
-  startTimedJob(id, 60).finally(()=>{
+  startTimedJob(id, 100).finally(() => {
     delete activeJobProgress[id];
-  })
+  });
+  console.log('Started job ' + id);
   res.status(200).json({ id });
 });
 
@@ -52,15 +70,20 @@ app.post('/api/jobs', (req, res) => {
 app.get('/api/jobs/:id/progress', (req, res) => {
   const id = req.params.id;
   const progress = activeJobProgress[id];
-  if(progress !== undefined){
-    res.status(200).json({ progress });
+  if (progress !== undefined) {
+    res.status(200).json({
+      id,
+      progress,
+    });
   } else {
     res.status(404).json({ error: 'Job not found' });
   }
-})
+});
 
 const port = process.env.port || 3333;
 
-server.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-}).on('error', console.error);
+server
+  .listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+  })
+  .on('error', console.error);
--- apps/rxjs-ws-demo/src/app/app.tsx
+++ apps/rxjs-ws-demo/src/app/app.tsx
@@ -1,12 +1,96 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+import { bind } from '@react-rxjs/core';
+import { map, ReplaySubject, scan } from 'rxjs';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3333');
+
+type Progress = {
+  id: string;
+  progress: number;
+};
+
+type DeleteProgress = {
+  id: string;
+  delete: true;
+};
+
+type ProgressMap = {
+  [id: string]: Progress;
+};
+
+const $jobsStream = new ReplaySubject<Progress | DeleteProgress>();
+
+socket.on('api/jobs/progress', (message) => {
+  $jobsStream.next(message);
+  console.log(message);
+});
+
+const $jobsMap = $jobsStream.pipe(
+  scan((acc, curr) => {
+    if ('delete' in curr) {
+      delete acc[curr.id];
+      return acc;
+    }
+    return {
+      ...acc,
+      [curr.id]: curr,
+    };
+  }, {} as ProgressMap)
+);
+
+// Will contain exactly one entry for each job because of the $jobsMap scan
+const $jobsList = $jobsMap.pipe(map((jobs) => Object.values(jobs)));
+
+const [useJobs] = bind($jobsList, []);
+
+const getJobProgress = async (id: string) => {
+  const response = await fetch(`http://localhost:3333/api/jobs/${id}/progress`);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response.json();
+};
+
+const startJob = async () => {
+  const response = await fetch('http://localhost:3333/api/jobs', {
+    method: 'POST',
+  });
+  const { id } = await response.json();
+  // optionally pre-fetch the job progress immedietly after starting it
+  const progress = await getJobProgress(id);
+  $jobsStream.next(progress);
+};
+
+const deleteJob = (id: string) => {
+  $jobsStream.next({ id, delete: true });
+};
+
+export function JobsList() {
+  const jobs = useJobs();
+  const jobsList = jobs.map((job) => {
+    const deleteJobButton =
+      job.progress >= 100 ? (
+        <button onClick={() => deleteJob(job.id)}>Delete Job</button>
+      ) : null;
+
+    return (
+      <div key={job.id}>
+        <div>{job.id}</div>
+        <div>{job.progress}</div>
+        {deleteJobButton}
+      </div>
+    );
+  });
+
+  return <div>{jobsList}</div>;
+}
 
 export function App() {
   return (
     <>
-      <NxWelcome title="rxjs-ws-demo" />
-      <div />
+      <button onClick={() => startJob().catch(console.error)}>Start job</button>
+      <JobsList />
     </>
   );
 }
